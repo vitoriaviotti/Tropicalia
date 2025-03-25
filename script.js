4dacd65b0be53786673f6b5c857de18ebbf14c4f
vitoriaviotti <137062011+vitoriaviotti@users.noreply.github.com>
2025-03-25 11:03:04 -0300

    s

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function){
+document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementByld('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuiFonteBotao');
 
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click', function){
+    aumentaFonteBotao.addEventListener('click', function(){
          tamanhoAtualFonte+=0.1;
          DocumentFragment.body.style.fontSize = '${tamanhoAtualFonte}rem';
 
     })/;
 
-    diminuiFonteBotao.addEventListener('click', function){
+    diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte-=0.1;
         document.body.style.fontSize= '${tamanhoAtualFonte}rem';
     });
